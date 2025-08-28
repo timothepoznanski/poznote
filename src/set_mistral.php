c9b1c5297a4b5bbc7f83838d00cf1bf91822f501
timothepoznanski <tim.poznanski@gmail.com>
2025-08-28 08:15:50 +0200

    chore: Remove deprecated API and workspace management files

--- src/set_mistral.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-require_once 'db_connect.php';
-
-$stmt = $con->prepare('UPDATE settings SET value = "mistral" WHERE key = "ai_provider"');
-$stmt->execute();
-echo 'AI provider set to Mistral' . "\n";
-?>
