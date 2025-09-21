973a7de89d8455ada1622f9c31e101db8ef71040
timothepoznanski <tim.poznanski@gmail.com>
2025-09-21 21:33:30 +0200

    style: remove unused index-inline.css and font-size-settings.css from head includes

--- src/templates/head_includes.php
+++ src/templates/head_includes.php
@@ -6,8 +6,6 @@
 <link type="text/css" rel="stylesheet" href="css/index.css"/>
 <link type="text/css" rel="stylesheet" href="css/modals.css"/>
 <link rel="stylesheet" href="css/index-mobile.css" media="(max-width: 800px)">
-<link rel="stylesheet" href="css/index-inline.css" />
-<link rel="stylesheet" href="css/font-size-settings.css" />
 <link rel="stylesheet" href="css/tasklist.css" />
 
 <!-- JavaScript Files -->
