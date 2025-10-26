6265d68ff63606bf598b7356adf2b5d751e25afb
timothepoznanski <tim.poznanski@gmail.com>
2025-10-26 08:39:12 +0100

    feat: Enhance table styles and add test table HTML for improved presentation

--- src/public_note.php
+++ src/public_note.php
@@ -108,7 +108,7 @@ $content = preg_replace_callback('#<([a-zA-Z0-9]+)([^>]*)>#', function($m) {
     <title>Shared note - <?php echo htmlspecialchars($note['heading'] ?: 'Untitled'); ?></title>
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/light.min.css">
-    <link rel="stylesheet" href="css/public_note.css">
+    <link rel="stylesheet" href="css/public_note.css?v=<?php echo filemtime(__DIR__ . '/css/public_note.css'); ?>">
     <link rel="stylesheet" href="css/tasks.css">
 </head>
 <body>
