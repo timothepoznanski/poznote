b2255d939c04724120c29fd7d87c961394a0374f
timothepoznanski <tim.poznanski@gmail.com>
2025-11-30 21:40:24 +0100

    feat: Add table context menu for enhanced table management in dark mode

--- src/index.php
+++ src/index.php
@@ -1045,6 +1045,7 @@ $body_classes = trim($extra_body_classes);
 <script src="js/tasklist.js?v=<?php echo $v; ?>"></script>
 <script src="js/excalidraw.js?v=<?php echo $v; ?>"></script>
 <script src="js/copy-code-on-focus.js?v=<?php echo $v; ?>"></script>
+<script src="js/table-context-menu.js?v=<?php echo $v; ?>"></script>
 
 <script>
 // Mobile navigation functionality
